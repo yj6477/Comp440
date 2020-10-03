220b04ce6e4b029eb298763002ba96eec96fe4d4
ys514111 <youngjin.seo.963@my.csun.edu>
2020-10-03 16:44:08 -0700

    some little changes

--- server.php
+++ server.php
@@ -15,16 +15,13 @@ if (isset($_POST['reg_user'])) {
   $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
   $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
-  // form validation: ensure that the form is correctly filled ...
-  // by adding (array_push()) corresponding error unto $errors array
   if (empty($username)) { array_push($errors, "Username is required"); }
 
   if (empty($password_1)) { array_push($errors, "Password is required"); }
   if ($password_1 != $password_2) {
 	array_push($errors, "The two passwords do not match");
   }
-  // form validation: ensure that the form is correctly filled ...
-  // by adding (array_push()) corresponding error unto $errors array
+  
   $user_check_query = "SELECT * FROM user WHERE username='$username'";
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
@@ -35,7 +32,6 @@ if (isset($_POST['reg_user'])) {
   }  
   // Finally, register user if there are no errors in the form
   if (count($errors) == 0) {
-  	//$password = md5($password_1);//encrypt the password before saving in the database
 
   	$query = "INSERT INTO user (user_id, username, password) 
   			  VALUES(NULL,'$username', '$password_1')";
@@ -58,7 +54,6 @@ if (isset($_POST['login_user'])) {
     }
   
     if (count($errors) == 0) {
-        //$password = md5($password);
         $query = "SELECT * FROM user WHERE username='$username' AND password='$password'";
         $results = mysqli_query($db, $query);
         if (mysqli_num_rows($results) == 1) {
